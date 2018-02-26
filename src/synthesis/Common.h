e2cd010939923e0012104674354cc6570b5ea6d6
Keliang He <keliang.h@gmail.com>
2018-02-26 14:20:39 -0600

    make directory follow cmake conventions

--- src/synthesis/Common.h
+++ src/synthesis/Common.h
@@ -2,7 +2,7 @@
 #include <vector>
 #include <string>
 #include <boost/algorithm/string.hpp>
-#include "../cudd-3.0.0/cplusplus/cuddObj.hh"
+#include "cuddObj.hh"
 #include <stdio.h>
 #include <stdlib.h>
 #include <set>
